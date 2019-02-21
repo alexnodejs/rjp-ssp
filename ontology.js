05ebbc840b17b25efecbd965dfb80758b93bd4d3
Glitch (plant-humor) <none>
2019-02-21 12:32:08 +0000

    🎗😹 Updated with Glitch

---
+++ ontology.js
@@ -0,0 +1,445 @@
+app.factory('ontology', function () {
+    return {
+        categories: [
+            {
+                "title": "Accounting & Consulting",
+                "icon": "briefcase",
+                "services": [
+                    {
+                        "title": "Accounting",
+                        "isPopular": true
+                    },
+                    {
+                        "title": "Financial Planning",
+                        "isPopular": true
+                    },
+                    {
+                        "title": "Management Consulting",
+                        "isPopular": true
+                    },
+                    {
+                        "title": "Human Resources"
+                    }
+                ]
+            },
+            {
+                "title": "Web, Mobile & Software Dev",
+                "icon": 'web-development',
+                "services": [
+                    {
+                        "title": "WordPress",
+                        "isPopular": true
+                    },
+                    {
+                        "title": "Software QA"
+                    },
+                    {
+                        "title": "Smart TV Development"
+                    },
+                    {
+                        "title": "Virtual Reality Development"
+                    },
+                    {
+                        "title": "Windows Development"
+                    },
+                    {
+                        "title": "Console Game Development"
+                    },
+                    {
+                        "title": "Browser Extension Development"
+                    },
+                    {
+                        "title": "Product Management"
+                    },
+                    {
+                        "title": "Chrome OS Development"
+                    },
+                    {
+                        "title": "Ecommerce Development"
+                    },
+                    {
+                        "title": "Embedded Software Development"
+                    },
+                    {
+                        "title": "Web Development",
+                        "isPopular": true
+                    },
+                    {
+                        "title": "Linux Development"
+                    },
+                    {
+                        "title": "Task Automation"
+                    },
+                    {
+                        "title": "Cryptography"
+                    },
+                    {
+                        "title": "MacOS Development"
+                    },
+                    {
+                        "title": "Creative Director"
+                    },
+                    {
+                        "title": "Design Research"
+                    },
+                    {
+                        "title": "Emerging Tech"
+                    },
+                    {
+                        "title": "Prototyping"
+                    },
+                    {
+                        "title": "Mobile Game Development",
+                        "isPopular": true
+                    },
+                    {
+                        "title": "Mobile App Development",
+                        "isPopular": true
+                    }
+                ]
+            },
+            {
+                "title": "Design & Creative",
+                "icon": "skills",
+                "services": [
+                    {
+                        "title": "Audio Production"
+                    },
+                    {
+                        "title": "Fashion Design"
+                    },
+                    {
+                        "title": "Voice Talent"
+                    },
+                    {
+                        "title": "2D Animation"
+                    },
+                    {
+                        "title": "Presentation Design"
+                    },
+                    {
+                        "title": "Graphic Design",
+                        "isPopular": true
+                    },
+                    {
+                        "title": "Illustration",
+                        "isPopular": true
+                    },
+                    {
+                        "title": "Musician"
+                    },
+                    {
+                        "title": "Video Production"
+                    },
+                    {
+                        "title": "Photography"
+                    },
+                    {
+                        "title": "Cartoonist"
+                    },
+                    {
+                        "title": "Art Director"
+                    },
+                    {
+                        "title": "3D Animation"
+                    },
+                    {
+                        "title": "Web Design",
+                        "isPopular": true
+                    },
+                    {
+                        "title": "Brand Strategy"
+                    },
+                    {
+                        "title": "Brand Identity Design"
+                    },
+                    {
+                        "title": "VR & AR Design"
+                    },
+                    {
+                        "title": "Music Production"
+                    },
+                    {
+                        "title": "Motion Graphics Design",
+                        "isPopular": true
+                    },
+                    {
+                        "title": "Mobile Design"
+                    },
+                    {
+                        "title": "Interior Design"
+                    },
+                    {
+                        "title": "Image Editing\/Retouching"
+                    },
+                    {
+                        "title": "Exhibit Design"
+                    },
+                    {
+                        "title": "Landscape Design"
+                    },
+                    {
+                        "title": "Scriptwriting"
+                    },
+                    {
+                        "title": "Videographer"
+                    },
+                    {
+                        "title": "Vocalist"
+                    },
+                    {
+                        "title": "Video Editing\/Post-Production"
+                    },
+                    {
+                        "title": "Store Design"
+                    },
+                    {
+                        "title": "Social Media Strategy"
+                    },
+                    {
+                        "title": "UX\/UI Design"
+                    }
+                ]
+            },
+            {
+                "title": "Legal",
+                "icon": 'legal',
+                "services": [
+                    {
+                        "title": "Website Agreements Law",
+                        "isPopular": true
+                    },
+                    {
+                        "title": "Business Formation"
+                    },
+                    {
+                        "title": "Finance & Securities Law",
+                        "isPopular": true
+                    },
+                    {
+                        "title": "Trademark & Copyright Law"
+                    },
+                    {
+                        "title": "Real Estate Law",
+                        "isPopular": true
+                    },
+                    {
+                        "title": "Lawsuits"
+                    },
+                    {
+                        "title": "Patent Law",
+                        "isPopular": true
+                    },
+                    {
+                        "title": "Immigration Law"
+                    },
+                    {
+                        "title": "Labor Law"
+                    },
+                    {
+                        "title": "Contract Law"
+                    },
+                    {
+                        "title": "Business Counsel"
+                    }
+                ]
+            },
+            {
+                "title": "Sales & Marketing",
+                "icon": "marketing-sales",
+                "services": [
+                    {
+                        "title": "Display Advertising"
+                    },
+                    {
+                        "title": "Search Engine Optimization",
+                        "isPopular": true
+                    },
+                    {
+                        "title": "Lead Generation",
+                        "isPopular": true
+                    },
+                    {
+                        "title": "Marketing Automation"
+                    },
+                    {
+                        "title": "Sales Representative"
+                    },
+                    {
+                        "title": "Public Relations"
+                    },
+                    {
+                        "title": "Marketing Strategy",
+                        "isPopular": true
+                    },
+                    {
+                        "title": "Market Research"
+                    },
+                    {
+                        "title": "Social Media Marketing",
+                        "isPopular": true
+                    },
+                    {
+                        "title": "Search Engine Marketing"
+                    }
+                ]
+            },
+            {
+                "title": "IT & Networking",
+                "icon": "it-programming",
+                "services": [
+                    {
+                        "title": "System Administration",
+                        "isPopular": true
+                    },
+                    {
+                        "title": "Information Security",
+                        "isPopular": true
+                    },
+                    {
+                        "title": "Database Administration",
+                        "isPopular": true
+                    }
+                ]
+            },
+            {
+                "title": "Translation",
+                "icon": "translators",
+                "services": [
+                    {
+                        "title": "Interpreting",
+                        "isPopular": true
+                    },
+                    {
+                        "title": "Localization",
+                        "isPopular": true
+                    },
+                    {
+                        "title": "Translation",
+                        "isPopular": true
+                    }
+                ]
+            },
+            {
+                "title": "Engineering & Architecture",
+                "icon": "engineering-architecture",
+                "services": [
+                    {
+                        "title": "Civil Engineering",
+                        "isPopular": true
+                    },
+                    {
+                        "title": "Chemical Engineering"
+                    },
+                    {
+                        "title": "Mechanical Engineering"
+                    },
+                    {
+                        "title": "Landscape Architecture",
+                        "isPopular": true
+                    },
+                    {
+                        "title": "Architecture",
+                        "isPopular": true
+                    },
+                    {
+                        "title": "Interdisciplinary Engineering"
+                    },
+                    {
+                        "title": "Industrial Design",
+                        "isPopular": true
+                    },
+                    {
+                        "title": "Electrical Engineering"
+                    }
+                ]
+            },
+            {
+                "title": "Admin Support",
+                "icon": "virtual-admin",
+                "services": [
+                    {
+                        "title": "Administrative Assistant",
+                        "isPopular": true
+                    },
+                    {
+                        "title": "Project Management",
+                        "isPopular": true
+                    },
+                    {
+                        "title": "Data Entry",
+                        "isPopular": true
+                    }
+                ]
+            },
+            {
+                "title": "Writing",
+                "icon": "writers",
+                "services": [
+                    {
+                        "title": "Academic Writing"
+                    },
+                    {
+                        "title": "Tutoring"
+                    },
+                    {
+                        "title": "Business Writing"
+                    },
+                    {
+                        "title": "Editing",
+                        "isPopular": true
+                    },
+                    {
+                        "title": "Curriculum Development"
+                    },
+                    {
+                        "title": "Article Writing"
+                    },
+                    {
+                        "title": "Genealogical Research"
+                    },
+                    {
+                        "title": "Creative Writing",
+                        "isPopular": true
+                    },
+                    {
+                        "title": "Technical Writing",
+                        "isPopular": true
+                    },
+                    {
+                        "title": "Copywriting",
+                        "isPopular": true
+                    }
+                ]
+            },
+            {
+                "title": "Data Science & Analytics",
+                "icon": "data-science-analytics",
+                "services": [
+                    {
+                        "title": "Data Science",
+                        "isPopular": true
+                    },
+                    {
+                        "title": "Data Mining",
+                        "isPopular": true
+                    },
+                    {
+                        "title": "Business Analysis",
+                        "isPopular": true
+                    }
+                ]
+            },
+            {
+                "title": "Customer Service",
+                "icon": "customer-service",
+                "services": [
+                    {
+                        "title": "Customer Service",
+                        "isPopular": true
+                    }
+                ]
+            }
+        ]
+    };
+});
